1c75a5ec2eaeba3c7adcb5b1554206e1c6a34981
Yoshiyasu KO <te108128@gmail.com>
2021-05-12 19:39:14 +0900

    implement todo_list_view_model

---
+++ lib/view_model/todo_list_view_model.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_app_with_provider/domain/data_repository.dart';
+import 'package:flutter_todo_app_with_provider/domain/todo_item.dart';
+
+class TodoListViewModel extends ChangeNotifier {
+  TodoListViewModel() {
+    todoItems = [];
+    fetch();
+  }
+  final DataRepository _dataRepository = DataRepository();
+  late List<TodoItem> todoItems;
+
+  void fetch() async {
+    _dataRepository.getAllTodoItems().then((items) {
+      todoItems = items;
+      notifyListeners();
+    });
+  }
+
+  void updateTodoItem(TodoItem item) async {
+    _dataRepository.updateTodoItem(item).then((_) {
+      fetch();
+    });
+  }
+
+  void deleteTodoItem(int id) async {
+    _dataRepository.deleteTodoItem(id).then((_) {
+      fetch();
+    });
+  }
+}
